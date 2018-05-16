9de014d1011cf0c41f50d112e0c4ddc33d76afe6
Nitesh <niteshgjoshi@gmail.com>
2018-05-15 18:03:48 -0700

    First Full Working Clock

--- script.js
+++ script.js
@@ -2,21 +2,25 @@ const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
-
-let hrPos = 90;
-let minPos = 276;
-let secPos = 132;
-
 var date = new Date();
 
-var hr = date.getHours()%12;
+let hr = date.getHours()%12;
 let min = date.getMinutes();
 let sec = date.getSeconds();
 
-secPos = sec*6;
-minPos = (min+(sec/60))*6;
-hrPos = (hr+((min+(sec/60))/60))*30;
+let secPos = sec*6;
+let minPos = (min+(sec/60))*6;
+let hrPos = (hr+((min+(sec/60))/60))*30;
 
-HOURHAND.style.transform = "rotate("+hrPos+"deg)";
-MINUTEHAND.style.transform = "rotate("+minPos+"deg)";
-SECONDHAND.style.transform = "rotate("+secPos+"deg)";
+function runTheClock()
+{
+	hrPos = (hrPos+(3/360))%360;
+    minPos = (minPos+(6/60))%360;
+    secPos = (secPos+6)%360;
+
+	HOURHAND.style.transform = "rotate("+hrPos+"deg)";
+	MINUTEHAND.style.transform = "rotate("+minPos+"deg)";
+	SECONDHAND.style.transform = "rotate("+secPos+"deg)";
+}
+
+var interval = setInterval(runTheClock,1000);
